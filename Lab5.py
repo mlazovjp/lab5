9c92a91e8a601a8d640e2322707866a97cbe88be
mlazovjp <noone@null.com>
2017-04-04 21:17:50 -0400

    Patches op code from 74 to 75

--- Lab5.py
+++ Lab5.py
@@ -1,1 +1,12 @@
 print "Hello Lab5\n"
+
+
+#need to patch byte 841 from 74 (jz) to 75 (jnz)
+
+## Reset to op code 74 (jz)
+PatchByte(0x401241, 0x74)
+print("BYTE @ 0x401241 is [0x%X]\n" % (Byte(0x401241)))
+
+#Patch op code to 75 (jnz)
+PatchByte(0x401241, 0x75)
+print("BYTE @ 0x401241 has been patched to [0x%X]\n" % (Byte(0x401241)))
